2441ebbd324801a93c429d9f63962bb0f2a78ee6
november-golf <n.s.gray@live.co.uk>
2022-11-03 16:16:06 +0000

    Complete Kata with explanation.

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-/* 
-👉 Write your kata here!
+/* COMMERCIAL CALAMITY - EPISODE ONE
+👉 You're working for a major ecommerce platform during the busiest time of year! 
+
+We had a system failure and we need to check our sales database for duplicate orders before
+the customers start hotting up the help desk.
+
+Write a function that checks if there are any duplicates in the array of orders.
+
+Return true if there are duplicates.
+Return false if there are no duplicates.
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
@@ -11,13 +19,18 @@ export function checkDuplicates(arr) {
   } else return false;
 }
 
+/* COMMERCIAL CALAMITY - EPISODE TWO
+👉 Great work! But now your boss is breathing down your neck and the pressure is on!
+We need to find out how many duplicates are in our sales database so that the cleanup teams 
+can sanitise them.
+
+Write a function that returns the number of duplicates in an array (if any).
+*/
+
+//👉 Write the function your CodeWarriors will start with below here:
 export function numberOfDupes(arr) {
     const newArr = [...new Set(arr)];
     return arr.length - newArr.length
 }
 
-// function removeDuplicates(arr) {
-//     return [...new Set(arr)];
-// }
 
-// console.log(removeDuplicates(arr))
